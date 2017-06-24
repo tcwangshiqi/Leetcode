2a9a43a6e44d3ba73cb4fea375309617dbe9e1ea
shiqi wang <2582730681@qq.com>
2017-06-24 17:12:16 +0800

    edit the two_sum

---
+++ two_sum/solution.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	int[] nums = [2,7,5,8];
+	int numsSize = nums.length;
+	int target = 9;
+	int[] value = new int[2];
+	value = twoSum(nums, numsSize, target);
+	printf("%i,%i",value[0],value[1]);
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target) {
+    int* value;
+    value = (int*)malloc(2*sizeof(int));
+    for(int i=0;i<numsSize;i++) {
+        for(int j=i+1;j<numsSize;j++) {
+            if((nums[i] + nums[j]) == target) {
+                value[0] = i;
+                value[1] = j;
+            }
+        }
+    }
+    return value;
+}
